6b2b812405b01df8588212209bbaebaa78168255
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-22 14:12:35 +0800

    update here:

--- Register/register.php
+++ Register/register.php
@@ -1,23 +1,38 @@
 <?php
-session_start();
-require_once('db.php');
+// Assuming you have a database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_nt3102";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $full_name = $_POST['full_name'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $role = $_POST['role'];
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Use prepared statements to prevent SQL injection
-    $stmt = $conn->prepare('INSERT INTO users (full_name, username, password, role) VALUES (?, ?, ?, ?)');
-    $stmt->bind_param('ssss', $full_name, $username, $password, $role);
-
-    if ($stmt->execute()) {
-        echo "User registered successfully!";
-    } else {
-        echo "Error during registration: " . $stmt->error;
-    }
-
-    $stmt->close();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Retrieve data from the form
+$lastname = $_POST['lastname'];
+$firstname = $_POST['firstname'];
+$department = $_POST['department'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$usersign = $_POST['usersign'];
+
+// Insert into tbemployee table
+$sql = "INSERT INTO tbemployee (lastname, firstname, department) VALUES ('$lastname', '$firstname', '$department')";
+$conn->query($sql);
+
+// Retrieve the generated empid
+$last_empid = $conn->insert_id;
+
+// Insert into security table with the generated empid
+$sql = "INSERT INTO security (username, password, role, usersign, empid) VALUES ('$username', '$password', 'security', '$usersign', '$last_empid')";
+if ($conn->query($sql) === TRUE) {
+    echo "User registered successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
